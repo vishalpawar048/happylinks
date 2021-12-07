5d5ba13129c709d6bc24041e92c9a0ca56bd2e22
Vishal Pawar <vishalpawar@Vishals-MacBook-Air.local>
2021-12-07 16:45:43 +0530

    ui fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/Layout";
+import Layout from "../components/Layout";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
