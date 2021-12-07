cb8fc0aa57252b5e1cda939ed6b0f19990eb1537
Vishal Pawar <vishalpawar@Vishals-MacBook-Air.local>
2021-12-07 17:47:37 +0530

    components moved new

--- service/firebaseApp.js
+++ service/firebaseApp.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAuk60C9UewMDPQZXysz1kCI9srgyP2uVk",
+  authDomain: "bruhh-46e72.firebaseapp.com",
+  projectId: "bruhh-46e72",
+  storageBucket: "bruhh-46e72.appspot.com",
+  messagingSenderId: "1031120641195",
+  appId: "1:1031120641195:web:7cb3cafb288b763ab0abee",
+  measurementId: "G-FE1SQR81NC",
 };
 
 // const analytics = getAnalytics(firebaseApp);
