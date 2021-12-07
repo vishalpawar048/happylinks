b7948fad213b5072b1ca85f532613d887a2a06ae
Vishal Pawar <vishalpawar@Vishals-MacBook-Air.local>
2021-12-07 17:40:45 +0530

    components moved

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,22 @@
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
 export default function Post({ postData }) {
   return (
     <div></div>
